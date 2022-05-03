43fafb94a02bd431728d0c9382aea33e2523dd07
Ivan <39461389+AlcibiadesCleinias@users.noreply.github.com>
2022-05-03 14:23:34 +0300

    closes #10 (#14)
    * closes #10
    
    * add while True

--- api/src/config/misc.py
+++ api/src/config/misc.py
@@ -1,5 +1,7 @@
 import aioredis
+from web3 import Web3
 
 from config.settings import settings
 
 redis = aioredis.from_url(f'redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}', db=0, decode_responses=True)
+w3 = Web3(Web3.HTTPProvider(settings.INFURA_HTTPS_ENDPOINT, request_kwargs={'timeout': 60}))
